9046293dae6626ed7963f0cbc360491795d65bfd
Krolik23 <szymon.krolikowski@outlook.com>
2017-09-04 00:44:44 +0200

    Complete interface for breathalyzer project

---
+++ aditional_lib/helperFunctionsLib.h
@@ -0,0 +1,19 @@
+#include "stm32f4xx_tim.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_adc.h"
+#include "stm32f4xx_rcc.h"
+#include "lcd_5110.h"
+#include <math.h>
+
+void windowFrame1();
+void windowFrame2();
+void setTimer();
+void reverseWindowFrame();
+void heatUpProgress(int i);
+void adc1Init();
+void printAdcValue();
+float printFloatAdcValue();
+void bloodAlcLevelShow();
+void waitingScreenShow();
+void breathAlyzerLogoShow();
+void howLongToDrive(float floatValuePerMil);
